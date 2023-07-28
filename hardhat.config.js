380e3afc309ffd136be23fd8173e6c1ac5b8f0b4
Yash Garg <yashygkf@gmail.com>
2023-07-28 11:34:25 +0530

    deploy script added

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,66 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+
 module.exports = {
-  solidity: "0.8.18",
+  solidity: {
+    compilers: [
+      {
+        version: "0.5.13",
+        settings: {
+          evmVersion: "istanbul",
+          optimizer: { enabled: true, runs: 200 }
+        }
+      },
+      {
+        version: "0.6.9",
+        settings: {
+          optimizer: { enabled: true, runs: 200 }
+        }
+      },
+      {
+        version: "0.7.6",
+        settings: {
+          evmVersion: "istanbul",
+          optimizer: { enabled: true, runs: 200 }
+        }
+      },
+      {
+        version: "0.5.12",
+        settings: {
+          optimizer: { enabled: true, runs: 200 }
+        }
+      },
+      {
+        version: "0.6.12",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        }
+      },
+      {
+        version: "0.6.0"
+      },
+      {
+        version: "0.8.0"
+      },
+      {
+        version: "0.8.2"
+      }
+    ]
+  },
+  networks: {
+    okbcTestnet: {
+      url: "https://okbtestrpc.okbchain.org",
+      accounts: [""],
+      chainId: 195,
+    },
+  },
+  // etherscan: {
+  //   apiKey: {
+  //     oklinkExplorer: POLYGON_SCAN_KEY,
+  //   },
+  // },
 };
